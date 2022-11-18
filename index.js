aec99e1b598fec7f5e069c94938775957b605267
James Noria <james.noriavelarde@tcs.com>
2022-11-18 15:28:57 -0500

    event on fire

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const contactButton = document.getElementById('text-for-ga4');
 
 contactButton.addEventListener('click', (e) => {
   e.preventDefault();
+  // Send event to GA4
+  gtag('event', 'click', {
+    event_category: 'contact',
+    event_label: 'contact button',
+    value: 1,
+  });
   console.log('Contact button clicked');
 });
